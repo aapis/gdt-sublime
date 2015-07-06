00fba42171e7a6d23ebb8d3ac87081d6205faee0
Ryan Priebe <rpriebe@me.com>
2015-07-05 20:51:05 -0600

    select the date right within the sublime interface

--- github.py
+++ github.py
@@ -4,12 +4,12 @@ from commander import Commander
 
 class GranifyGithubMergedTodayCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
+		window = self.view.window()
 		command = "granify github merged_today"
 		general = Commander(command)
 		response = general.send_order(self.__class__.__name__)
 		
 		if(response[0]):
-			window = self.view.window()
 			log_window = window.new_file()
 			log_window.insert(edit, 0, response[1])
 			log_window.set_name("Pull Requests Merged Today")
@@ -19,24 +19,32 @@ class GranifyGithubMergedTodayCommand(sublime_plugin.TextCommand):
 
 class GranifyGithubMergedOnCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		command = "granify github merged_on"
-		general = Commander(command)
-		response = general.send_order(self.__class__.__name__)
+		window = self.view.window()
+		window.show_input_panel("Date:", "", self.on_done, None, None)
 		
-		if(response[0]):
-			window = self.view.window()
-			log_window = window.new_file()
-			log_window.insert(edit, 0, response[1])
-			log_window.set_name("Pull Requests Merged On {DATE}")
-			log_window.set_read_only(True)
-		else:
-			sublime.message_dialog("No PRs were merged on that day")
+	def on_done(self, date):
+		if(date != None):
+			command = "granify github merged_on --start=%s" % date
+			general = Commander(command)
+			response = general.send_order(self.__class__.__name__)
+			
+			if(response[0]):
+				window = self.view.window()
+				log_window = window.new_file()
+				log_window.insert(edit, 0, response[1])
+				log_window.set_name("Pull Requests Merged On {DATE}")
+				log_window.set_read_only(True)
+			else:
+				sublime.message_dialog("No PRs were merged on that day")
 
 class GranifyGithubMergedBetweenCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		sublime.status_message("Testing!")
-
-		command = "granify github merged_between"
+		window = self.view.window()
+		window.show_input_panel("Date Range:", "", self.on_done, None, None)
+	
+	def on_done(self, date_range):
+		date_range = date_range.split(' to ')
+		command = "granify github merged_between --start=%s --end=%s" % (date_range[0], date_range[1])
 		general = Commander(command)
 		response = general.send_order(self.__class__.__name__)
 		
