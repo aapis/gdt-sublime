44d7f0830dd62947d97aec6370646b6862f8a62f
Ryan Priebe <rpriebe@me.com>
2015-07-06 23:14:29 -0600

    implemented WAIT_FOR_THREAD constant to determine whether the commands will wait or just assume things are happening in the background

--- granify.py
+++ granify.py
@@ -5,30 +5,44 @@ import os.path
 import Queue
 from commander import Commander
 
+WAIT_FOR_THREAD = False
+
 class GranifyRecompileCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		sublime.status_message("Recompiling granify/goliath")
-		threads = {}
 
 		command = "granify recompile"
 		queue = Queue.Queue()
 		general = Commander(command, queue)
 		general.start()
-		# command_executed, message = queue.get()
 
-		# if(command_executed):
-		# 	sublime.message_dialog("Granify and Goliath were recompiled")
-		# else:
-		# 	sublime.error_message("Problem recompiling granify/goliath")
+		if WAIT_FOR_THREAD:
+			# The following code gets the response from the executed command, it's more
+			# accurate but also requires you to wait for the thread to finish
+			command_executed, message = queue.get()
+
+			if(command_executed):
+				sublime.message_dialog("Granify and Goliath were recompiled")
+			else:
+				sublime.error_message("Problem recompiling granify/goliath")
+		else:
+			sublime.message_dialog("Recompiling in progress")
+		
 
 class GranifyStartupCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		command = "granify startup both"
 		queue = Queue.Queue()
 		general = Commander(command, queue)
-		command_executed, message = queue.get()
 
-		if(command_executed):
-			sublime.message_dialog("Granify and Goliath started")
+		if WAIT_FOR_THREAD:
+			# The following code gets the response from the executed command, it's more
+			# accurate but also requires you to wait for the thread to finish
+			command_executed, message = queue.get()
+
+			if(command_executed):
+				sublime.message_dialog("Granify and Goliath started")
+			else:
+				sublime.error_message("Problem starting granify/goliath")
 		else:
-			sublime.error_message("Problem starting granify/goliath")
+			sublime.message_dialog("Startup in progress")
